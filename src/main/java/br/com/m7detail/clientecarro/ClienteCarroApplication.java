08104ca648e57df1c61014c80c8b6cc7313ff399
Mardson Santana <mardsonsantana105@gmail.com>
2022-12-25 23:23:39 -0300

    inicia projeto

---
+++ src/main/java/br/com/m7detail/clientecarro/ClienteCarroApplication.java
@@ -0,0 +1,13 @@
+package br.com.m7detail.clientecarro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClienteCarroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClienteCarroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/m7detail/clientecarro/ClienteCarroApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.m7detail.clientecarro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClienteCarroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
