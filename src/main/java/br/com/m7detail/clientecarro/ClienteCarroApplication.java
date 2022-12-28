990aeda46c1df8d816b880333771728ee2af084c
Mardson Santana <mardsonsantana105@gmail.com>
2022-12-27 23:15:26 -0300

    implementa api de teste

--- src/main/java/br/com/m7detail/clientecarro/ClienteCarroApplication.java
+++ src/main/java/br/com/m7detail/clientecarro/ClienteCarroApplication.java
@@ -2,9 +2,21 @@ package br.com.m7detail.clientecarro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class ClienteCarroApplication {
+	
+	
+	@GetMapping
+	public String getHomeTeste() {
+		return "Cliente Carro - API Home"; 
+	}
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(ClienteCarroApplication.class, args);
