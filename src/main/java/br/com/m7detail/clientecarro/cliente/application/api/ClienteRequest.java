9f4bca0584be18bc665535f28caf4bcff6a7ea6d
Mardson Santana <mardsonsantana105@gmail.com>
2022-12-29 15:50:10 -0300

    implementa request e response

--- src/main/java/br/com/m7detail/clientecarro/cliente/application/api/ClienteRequest.java
+++ src/main/java/br/com/m7detail/clientecarro/cliente/application/api/ClienteRequest.java
@@ -1,5 +1,23 @@
 package br.com.m7detail.clientecarro.cliente.application.api;
 
+import org.hibernate.validator.constraints.br.CPF;
+
+import br.com.m7detail.clientecarro.cliente.domain.Sexo;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Value;
+
+@Value
 public class ClienteRequest {
+	@NotBlank
+	private String nomeCompleto;
+	@NotBlank
+	@Email
+	private String email;
+	@NotBlank
+	private String celular;
+	private Sexo sexo;
+	@CPF
+	private String cpf;
 
 }
--- src/main/java/br/com/m7detail/clientecarro/cliente/application/api/ClienteResponse.java
+++ src/main/java/br/com/m7detail/clientecarro/cliente/application/api/ClienteResponse.java
@@ -1,5 +1,11 @@
 package br.com.m7detail.clientecarro.cliente.application.api;
 
+import java.util.UUID;
+
+import lombok.Value;
+
+@Value
 public class ClienteResponse {
+	private UUID idCliente;
 
 }
