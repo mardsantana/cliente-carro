518f983c30c96fbb63aaef32847c07ccd85b65bf
Mardson Santana <mardsonsantana105@gmail.com>
2022-12-30 11:40:21 -0300

    implementa repository. Teste ok!

--- src/main/java/br/com/m7detail/clientecarro/cliente/infra/ClienteInfraRepository.java
+++ src/main/java/br/com/m7detail/clientecarro/cliente/infra/ClienteInfraRepository.java
@@ -13,6 +13,7 @@ public class ClienteInfraRepository implements ClienteRepository {
 	@Override
 	public Cliente salva(Cliente cliente) {
 		log.info("[inicia] ClienteInfraRepository - salva");
+		clienteSpringDataJPARepository.salva(cliente);
 		log.info("[finaliza] ClienteInfraRepository - salva");
 		return cliente;
 	}
