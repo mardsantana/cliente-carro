723b479d96228aa7551086d8f8777d922220b568
Mardson Santana <mardsonsantana105@gmail.com>
2023-01-03 23:19:18 -0300

    implementa fluxo Repository. Test ok!

--- src/main/java/br/com/m7detail/clientecarro/cliente/application/repository/ClienteRepository.java
+++ src/main/java/br/com/m7detail/clientecarro/cliente/application/repository/ClienteRepository.java
@@ -1,8 +1,11 @@
 package br.com.m7detail.clientecarro.cliente.application.repository;
 
+import java.util.List;
+
 import br.com.m7detail.clientecarro.cliente.domain.Cliente;
 
 public interface ClienteRepository {
 	Cliente salva(Cliente cliente);
+	List<Cliente> buscaTodosClientes();
 
 }
--- src/main/java/br/com/m7detail/clientecarro/cliente/infra/ClienteInfraRepository.java
+++ src/main/java/br/com/m7detail/clientecarro/cliente/infra/ClienteInfraRepository.java
@@ -1,5 +1,7 @@
 package br.com.m7detail.clientecarro.cliente.infra;
 
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
 
 import br.com.m7detail.clientecarro.cliente.application.repository.ClienteRepository;
@@ -18,6 +20,13 @@ public class ClienteInfraRepository implements ClienteRepository {
 		clienteSpringDataJPARepository.save(cliente);
 		log.info("[finaliza] ClienteInfraRepository - salva");
 		return cliente;
+	}
+	@Override
+	public List<Cliente> buscaTodosClientes() {
+		log.info("[inicia] ClienteInfraRepository - buscaTodosClientes");
+		List<Cliente> todosClientes = clienteSpringDataJPARepository.findAll();
+		log.info("[finaliza] ClienteInfraRepository - buscaTodosClientes");
+		return todosClientes;
 	}
 
 }
